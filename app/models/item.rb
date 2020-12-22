bd1e9fe2bcadc8e6f96d30ef31903bc9604cafa7
nakadakeisuke <74888526+nakadakeisuke@users.noreply.github.com>
2020-12-22 16:39:19 +0900

    アソシエーション記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_day
 end
