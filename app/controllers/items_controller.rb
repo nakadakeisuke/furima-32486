fa12103dc19b499f81622b13680c3d53d3d4f3f8
nakadakeisuke <74888526+nakadakeisuke@users.noreply.github.com>
2020-12-25 19:40:14 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new]
+  before_action :authenticate_user!, only: [:create, :new, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,6 +19,23 @@ class ItemsController < ApplicationController
        redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @item =Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
     end
   end
 
